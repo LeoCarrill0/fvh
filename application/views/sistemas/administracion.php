0da576f8e4ce2bf5c21c9f47be6018251dc17beb
Leo Carrillo <leonardo.ltcg63@gmail.com>
2022-02-22 22:29:59 -0600

    commit init

--- application/views/sistemas/administracion.php
+++ application/views/sistemas/administracion.php
@@ -17,6 +17,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <!DOCTYPE html>
 <html lang="es">
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
